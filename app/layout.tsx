e60cb98b7c3c7c701d3566007c6f6e682ece768a
Vadzim Simanau <eks220885@gmail.com>
2025-08-24 12:59:53 +0300

    TanStackProvide outside, AuthProvider inside

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,51 +1,51 @@
 // app/layout.tsx
-import styles from "./page.module.css";
-import "./globals.css";
-import type { Metadata } from "next";
-import Header from "@/components/Header/Header";
-import Footer from "@/components/Footer/Footer";
-import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
-import AuthProvider from "@/components/AuthProvider/AuthProvider";
-import { Roboto } from "next/font/google";
-import homeStyles from "./Home.module.css";
-import { Tag } from "@/types/note";
+import styles from './page.module.css';
+import './globals.css';
+import type { Metadata } from 'next';
+import Header from '@/components/Header/Header';
+import Footer from '@/components/Footer/Footer';
+import TanStackProvider from '@/components/TanStackProvider/TanStackProvider';
+import AuthProvider from '@/components/AuthProvider/AuthProvider';
+import { Roboto } from 'next/font/google';
+import homeStyles from './Home.module.css';
+import { Tag } from '@/types/note';
 
 const roboto = Roboto({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-  variable: "--font-roboto",
-  display: "swap",
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-roboto',
+  display: 'swap',
 });
 
 export const metadata: Metadata = {
-  title: "NoteHub",
-  description: "Your Personal Note Manager to manage your notes efficiently.",
+  title: 'NoteHub',
+  description: 'Your Personal Note Manager to manage your notes efficiently.',
   openGraph: {
-    title: "NoteHub",
-    description: "Your Personal Note Manager to manage your notes efficiently.",
-    url: "https://08-zustand-swart-three.vercel.app",
+    title: 'NoteHub',
+    description: 'Your Personal Note Manager to manage your notes efficiently.',
+    url: 'https://08-zustand-swart-three.vercel.app',
     images: [
       {
-        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+        url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
         width: 1200,
         height: 630,
-        alt: "NoteHub Preview",
+        alt: 'NoteHub Preview',
       },
     ],
   },
 };
 
 const allTags: Tag[] = [
-  "Work",
-  "Personal",
-  "Meeting",
-  "Shopping",
-  "Ideas",
-  "Travel",
-  "Finance",
-  "Health",
-  "Important",
-  "Todo",
+  'Work',
+  'Personal',
+  'Meeting',
+  'Shopping',
+  'Ideas',
+  'Travel',
+  'Finance',
+  'Health',
+  'Important',
+  'Todo',
 ];
 
 export default function RootLayout({
@@ -58,18 +58,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${roboto.variable} ${styles.body}`}>
-        <AuthProvider>
-          <TanStackProvider>
-            {}
-            {}
+        <TanStackProvider>
+          <AuthProvider>
             <Header allTags={allTags} />
             <main className={homeStyles.main}>{children}</main>
             <div id="modal-root" />
             {modal}
             <Footer />
-            {}
-          </TanStackProvider>
-        </AuthProvider>
+          </AuthProvider>
+        </TanStackProvider>
       </body>
     </html>
   );
