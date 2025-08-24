f8f8dd802bcd10d0ce6512c7bd07aeeb0397b886
Vadzim Simanau <eks220885@gmail.com>
2025-08-24 14:25:01 +0300

    fetchNoteById from @/lib/api/serverApi

--- app/(private routes)/notes/[id]/page.tsx
+++ app/(private routes)/notes/[id]/page.tsx
@@ -1,9 +1,9 @@
-// app/notes/[id]/page.tsx
-import { QueryClient, dehydrate } from "@tanstack/react-query";
-import NoteDetailsClient from "@/app/(private routes)/notes/[id]/NoteDetails.client";
-import { fetchNoteById } from "@/lib/api/clientApi";
-import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
-import { type Metadata } from "next";
+// app/(private routes)/notes/[id]/page.tsx
+import { QueryClient, dehydrate } from '@tanstack/react-query';
+import NoteDetailsClient from '@/app/(private routes)/notes/[id]/NoteDetails.client';
+import { fetchNoteById } from '@/lib/api/serverApi';
+import TanStackProvider from '@/components/TanStackProvider/TanStackProvider';
+import { type Metadata } from 'next';
 
 export async function generateMetadata({
   params,
@@ -13,49 +13,50 @@ export async function generateMetadata({
   const resolvedParams = await params;
   const { id } = resolvedParams;
 
-  try {
-    const note = await fetchNoteById(id);
-    const description =
-      note.content.length > 100
-        ? note.content.slice(0, 97) + "..."
-        : note.content;
+  const note = await fetchNoteById(id);
 
+  if (!note) {
     return {
-      title: `NoteHub - ${note.title}`,
-      description: `Read more about ${note.title}: ${description}`,
+      title: 'NoteHub - Note Not Found',
+      description: 'The note you are looking for does not exist on NoteHub.',
       openGraph: {
-        title: `NoteHub - ${note.title}`,
-        description: `Read more about ${note.title}: ${description}`,
+        title: 'NoteHub - Note Not Found',
+        description: 'The note you are looking for does not exist on NoteHub.',
         url: `https://08-zustand-swart-three.vercel.app/notes/${id}`,
         images: [
           {
-            url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+            url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
             width: 1200,
             height: 630,
-            alt: "NoteHub Preview",
-          },
-        ],
-      },
-    };
-  } catch {
-    return {
-      title: "NoteHub - Note Not Found",
-      description: "The note you are looking for does not exist on NoteHub.",
-      openGraph: {
-        title: "NoteHub - Note Not Found",
-        description: "The note you are looking for does not exist on NoteHub.",
-        url: `https://08-zustand-swart-three.vercel.app/notes/${id}`,
-        images: [
-          {
-            url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
-            width: 1200,
-            height: 630,
-            alt: "NoteHub Preview",
+            alt: 'NoteHub Preview',
           },
         ],
       },
     };
   }
+
+  const description =
+    note.content.length > 100
+      ? note.content.slice(0, 97) + '...'
+      : note.content;
+
+  return {
+    title: `NoteHub - ${note.title}`,
+    description: `Read more about ${note.title}: ${description}`,
+    openGraph: {
+      title: `NoteHub - ${note.title}`,
+      description: `Read more about ${note.title}: ${description}`,
+      url: `https://08-zustand-swart-three.vercel.app/notes/${id}`,
+      images: [
+        {
+          url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+          width: 1200,
+          height: 630,
+          alt: 'NoteHub Preview',
+        },
+      ],
+    },
+  };
 }
 
 export default async function NoteDetailsPage({
@@ -69,7 +70,7 @@ export default async function NoteDetailsPage({
   const queryClient = new QueryClient();
 
   await queryClient.prefetchQuery({
-    queryKey: ["note", id],
+    queryKey: ['note', id],
     queryFn: () => fetchNoteById(id),
   });
 
